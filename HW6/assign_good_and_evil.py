a9bbb2385102e0f9119a776153c2d2b04de7960c
alicewang10t <alw033@ucsd.edu>
2025-11-12 23:23:39 -0800

    Update assign_good_and_evil.py

--- HW6/assign_good_and_evil.py
+++ HW6/assign_good_and_evil.py
@@ -2,6 +2,26 @@ def assign_good_and_evil(graph):
     '''
     Assigns good and evil labels to nodes in a graph.
     '''
-    # TODO: Implement the assign_good_and_evil function
-    
-    return None
+    dict = {}
+    for source in graph.nodes():
+        if source not in dict:
+            queue = deque([source])
+            dict[source] = 'good'
+            
+            while pending:
+                u = pending.popleft()
+                first = dict[u]
+                if first == 'good':
+                    second = 'evil'
+                else: 
+                    second = 'good'
+                
+                for v in graph.neighbors(u):
+                    if v not in dict:
+                        dict[v] = second
+                        pending.append(v)
+                    elif dict[v] == first:
+                        return None
+                        
+    return labels
+
